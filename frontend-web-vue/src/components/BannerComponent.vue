e20b34cc47f83432cf6914536e56ec7e6793487e
andrinur13 <andribis13@gmail.com>
2022-03-29 21:31:17 +0700

    Home component
    Create home component start with banner

---
+++ frontend-web-vue/src/components/BannerComponent.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <!-- mobile -->
+    <div class="d-block d-sm-block d-md-block d-lg-none d-xl-none">
+      <!-- banner -->
+        <div>
+          <b-carousel id="carousel-fade" class="m-2" style="text-shadow: 0px 0px 2px #000" fade indicators img-width="1024" img-height="480">
+            <b-carousel-slide v-for="i in 3" :key="i.id" img-src="https://picsum.photos/1024/480/?image=10">
+            </b-carousel-slide>
+          </b-carousel>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BannerComponent"
+}
+</script>
+
+<style>
+.carousel-item>img {
+  border-radius: 10px;
+}
+
+.carousel-indicators {
+  bottom: -50px;
+}
+
+.carousel-indicators li {
+  background-color: #c4c4c4;
+}
+
+.carousel-indicators > .active {
+  background-color: #12569A;
+}
+</style>
--- frontend-web-vue/src/views/beranda/BerandaViews.vue
+++ frontend-web-vue/src/views/beranda/BerandaViews.vue
@@ -1,13 +1,27 @@
 <template>
-  
+  <div>
+      <!-- mobile view -->
+      <!-- header -->
+      <banner-component></banner-component>
+
+      <div class="row mt-4">
+        <div class="col">
+          <hr>
+        </div>
+      </div>
+  </div>
 </template>
 
 <script>
+import BannerComponent from '../../components/BannerComponent.vue'
 export default {
-
+  components: {
+    BannerComponent
+  }
 }
 </script>
 
 <style>
+
 
 </style>
