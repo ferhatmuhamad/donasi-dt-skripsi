001fe34e0d857c0b7c413fc5870b2c99491f01eb
andrinur13 <andribis13@gmail.com>
2022-04-02 22:56:31 +0700

    add font awesome
    Add font awesome to projects

--- frontend-web-vue/src/App.vue
+++ frontend-web-vue/src/App.vue
@@ -6,26 +6,5 @@
 
 
 <style>
-/* @import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"); */
-/* #app {
-  font-family: "Poppins", sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  font-weight: 500;
-  color: #2c3e50;
-} */
 
-/* #nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-} */
 </style>
--- frontend-web-vue/src/main.js
+++ frontend-web-vue/src/main.js
@@ -14,6 +14,11 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+// font awesome
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
