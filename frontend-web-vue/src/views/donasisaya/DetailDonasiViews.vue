07aa871621c7de7e8bb512bf0d48a6c9b9427e5d
andrinur13 <andribis13@gmail.com>
2022-04-03 14:55:28 +0700

    Donasi area done

---
+++ frontend-web-vue/src/views/donasisaya/DetailDonasiViews.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <div class="d-block d-sm-block d-md-block d-lg-none d-xl-none" style="margin-top: 70px; margin-bottom: 100px">
+      <navbar-top-component></navbar-top-component>
+      <navbar-bottom-component></navbar-bottom-component>
+      <div class="row">
+        <div class="col">
+          
+          <div class="row">
+            <div class="col text-center">
+              <div class="font-weight-bold" style="font-size: 20px">
+                Terima Kasih!
+              </div>
+              <div class="mt-2">
+                Donasi Anda telah Kami terima
+              </div>
+              <div class="mt-4">
+                <img class="img-fluid" style="max-height: 200px" src="https://cdn.dribbble.com/users/5126936/screenshots/14002963/media/f395c66c9f7455846001fe772ef14482.png?compress=1&resize=400x300&vertical=top" alt="">
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col mx-2 mt-4">
+              <div class="border">
+                <table class="table table-borderless text-center" style="font-size: 12px">
+                  <tr>
+                    <td>Tanggal</td>
+                    <td>19 September 2021</td>
+                  </tr>
+                  <tr>
+                    <td>ID Donasi</td>
+                    <td>#DNS202109190001</td>
+                  </tr>
+                  <tr>
+                    <td>Status</td>
+                    <td>Sukses</td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+          </div>
+
+          <div class="row mt-4">
+            <div class="col">
+              <hr>
+            </div>
+          </div>
+
+          <div class="container">
+            <div class="row">
+              <div class="col">
+                <div class="form-group mt-3">
+                  <div>Nominal Donasi</div>
+                  <div class="form-control bg-light mt-2">
+                    Rp. 700.000
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import NavbarBottomComponent from '../../components/NavbarBottomComponent.vue'
+import NavbarTopComponent from '../../components/NavbarTopComponent.vue'
+export default {
+  components: { NavbarTopComponent, NavbarBottomComponent },
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend-web-vue/src/views/donasisaya/DonasiViews.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div class="d-block d-sm-block d-md-block d-lg-none d-xl-none" style="margin-top: 70px; margin-bottom: 100px">
+      <navbar-top-component></navbar-top-component>
+      <navbar-bottom-component></navbar-bottom-component>
+
+      <!-- donasi views -->
+      <div class="row">
+        <div class="col">
+          <div class="row mx-1 border p-2 mt-2" style="border-radius: 5px" v-for="i in 5" :key="i">
+            <!-- <div class="col-auto">
+              <img class="img-fluid" style="max-width: 60px" src="https://www.logodesign.net/images/illustration-logo.png" alt="">
+            </div> -->
+            <div class="col">
+              <div style="font-size: 10px">
+                <div>
+                  Pembebasan Lahan Untuk Perluasan Area Yayasan Daarut Taqwa Ihsaniyya
+                </div>
+                <div class="mt-2">
+                  <div class="d-inline">17 Sept 2021</div>
+                  <div class="d-inline font-weight-bold ml-4">Rp. 150.000</div>
+                </div>
+              </div>
+            </div>
+            <div class="col-auto">
+              <div class="badge badge-warning" style="font-size: 8px">pending</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import NavbarBottomComponent from '../../components/NavbarBottomComponent.vue'
+import NavbarTopComponent from '../../components/NavbarTopComponent.vue'
+export default {
+  name: 'DonasiViews',
+  components: { NavbarTopComponent, NavbarBottomComponent },
+
+}
+</script>
+
+<style>
+
+</style>
