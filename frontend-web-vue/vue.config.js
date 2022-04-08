08129eed7a9f5bc9ff5b6cfc5a12ca4f35f454b1
andrinur13 <andribis13@gmail.com>
2022-04-08 21:25:05 +0700

    remove copywriting

---
+++ frontend-web-vue/vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    pwa: {
+        name: 'Daarut Taqwa Ihsaniyya',
+        themeColor: '#123C9A',
+
+        manifestOptions: {
+            background_color: "#123C9A"
+        }
+    }
+}
