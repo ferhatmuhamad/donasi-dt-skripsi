9925ced2fadae93118db72cdc3e22892dee55813
andrinur13 <andribis13@gmail.com>
2022-04-16 10:00:55 +0700

    prepare banner

--- backend-api/app/Http/Controllers/BannerController.php
+++ backend-api/app/Http/Controllers/BannerController.php
@@ -14,4 +14,20 @@ class BannerController extends Controller
 
         return format_response('success', Response::HTTP_OK, 'success fetch data banner', $databanner);
     }
+
+
+
+
+
+    // DASHBOARD ADMIN
+    public function index() {
+        $banner = BannerModel::get();
+
+        $data = [
+            'banner' => $banner
+        ];
+
+        return view('banner.index', $data);
+    }
+    // ENDDASHBOARD
 }
